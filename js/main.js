f74d02707802c45045eca36d2e7dede530600f46
KanittaJHA <kanittaketin@gmail.com>
2025-08-08 01:16:48 +0700

    Refactor main page: updated HTML, CSS, and JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 /*=============== SHOW MENU ===============*/
+// ดีง Element ที่เกี่ยวข้องกับข้อมูลมาเก็บไว้ในตัวแปร
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+// MENU SHOW
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// MENU HIDE
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SWIPER WACTHES ===============*/
 
